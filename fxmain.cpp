eb9825a6713115be682f0c0dc739ee84c3370838
sergeydon <sergey.expert.spb@gmail.com>
2016-10-25 18:33:28 +0300

    fix 25102016

--- fxmain.cpp
+++
@@ -1,7 +0,0 @@
-#include "fxmain.h"
-
-
-
-fxmain::fxmain()
-{
-}
--- main.cpp
+++ main.cpp
@@ -60,12 +60,14 @@ struct MqlOrder
 };
 #pragma pack(pop,1)
 
-MqlOrder actList[64];
-
+struct MqlOrderAction
+{
+	int			action;
+	int			ticket;
+};
 
 #pragma data_seg ("shared")
 FxcOrder orders[MAX_ORDER_COUNT] = { 0 };
-FxcOrder orders_old[MAX_ORDER_COUNT] = { 0 };
 int ordersCount = 0;
 int ordersTotal = 0;
 bool ordersValid = false;
@@ -73,25 +75,16 @@ bool transmitterBusy = false;
 #pragma data_seg()
 #pragma comment(linker, "/SECTION:shared,RWS")
 
+FxcOrder orders_old[MAX_ORDER_COUNT] = { 0 };
+MqlOrderAction actionOrder[MAX_ORDER_COUNT] = { 0 };
+int ordersRCount = 0;
+
 namespace fxc {
 	///Копируем строку с++ в MQLSting (на стороне MQL больше ничего делать не надо)
 	inline void writeMqlString(MqlString dest, wchar_t* source) {
 		int len = min(wcslen(source), dest.size - 1);  //Определяем длину строки (небольше распределенного буфера)
 		wcscpy_s(dest.buffer, len + 1, source);  //Копируем включая терминирующий ноль
 		*(((int*)dest.buffer) - 1) = len;  // Записываем длину строки (хак, может измениться в будующих версиях терминала)
-	}
-
-
-	//+------------------------------------------------------------------+
-	//| В текстовой строке заменяем подстроку на подстроку               |
-	//| string передается в виде прямой ссылки на контент строки         |
-	//+------------------------------------------------------------------+
-	void fnReplaceString(wchar_t *text, wchar_t *from)
-	{
-		//--- проверка параметров
-		if (text == NULL || from == NULL) return;
-		//--- копирование
-		memcpy(text, from, wcslen(from)*sizeof(wchar_t));
 	}
 
 	Poco::NamedMutex mutex("ffc_mutex");
@@ -107,7 +100,7 @@ namespace fxc {
 				SetConsoleCP(CP_UTF8);
 				std::cout << "DLL inited.\r\n";
 			}
-			is_init = true; 
+			is_init = true;
 			return 1;
 		}
 		return 0;
@@ -124,7 +117,7 @@ namespace fxc {
 
 		wcscpy_s(orders[ordersCount].symbol, SYMBOL_LENGTH, OrderSymbol);
 		wcscpy_s(orders[ordersCount].comment, COMMENT_LENGTH, OrderComment);
-		
+
 
 		std::wcout << "order #" << ordersCount << " " << OrderTicket << " " << orders[ordersCount].symbol << "\r\n";
 		ordersCount++;
@@ -135,21 +128,21 @@ namespace fxc {
 		for (int i = 0; i < ordersTotal; i++) {
 			MqlOrder* copy_order = master_array + i;
 			FxcOrder* master_order = orders + i;
-		
-			copy_order->ticket		= master_order->ticket;
-			copy_order->magic		= master_order->magic;
-			copy_order->type		= master_order->type;
-			copy_order->lots		= master_order->lots;
-			copy_order->openprice	= master_order->openprice;
-			copy_order->opentime	= master_order->opentime;
-			copy_order->tpprice		= master_order->tpprice;
-			copy_order->slprice		= master_order->slprice;
-			copy_order->closeprice	= master_order->closeprice;
-			copy_order->closetime	= master_order->closetime;
-			copy_order->expiration	= master_order->expiration;
-			copy_order->profit		= master_order->profit;
-			copy_order->comission	= master_order->comission;
-			copy_order->swap		= master_order->swap;
+
+			copy_order->ticket = master_order->ticket;
+			copy_order->magic = master_order->magic;
+			copy_order->type = master_order->type;
+			copy_order->lots = master_order->lots;
+			copy_order->openprice = master_order->openprice;
+			copy_order->opentime = master_order->opentime;
+			copy_order->tpprice = master_order->tpprice;
+			copy_order->slprice = master_order->slprice;
+			copy_order->closeprice = master_order->closeprice;
+			copy_order->closetime = master_order->closetime;
+			copy_order->expiration = master_order->expiration;
+			copy_order->profit = master_order->profit;
+			copy_order->comission = master_order->comission;
+			copy_order->swap = master_order->swap;
 
 			writeMqlString(copy_order->symbol, master_order->symbol);
 			writeMqlString(copy_order->comment, master_order->comment);
@@ -167,13 +160,6 @@ namespace fxc {
 	int ffc_GetOrderInfo() {
 		MqlOrder mqlGetOrder[1] = { orders[ordersCount].ticket, orders[ordersCount].magic };
 		return 1;
-	}
-
-
-
-
-	void ffc_DeInit() {
-		is_init = false;
 	}
 
 	void ffc_ordersCount(int num) {
@@ -202,4 +188,60 @@ namespace fxc {
 	int ffc_EndSession() {
 		return true;
 	}
+
+	void ffc_DeInit() {
+		is_init = false;
+	}
+
+
+
+	// ------------------------------------------------------------------- //
+	// ----------------------- Receiver Part ----------------------------- //
+	// ------------------------------------------------------------------- //
+
+	int ffc_ROrdersUpdate(int ROrderTotal, int ROrderTicket, int Rmagic, wchar_t* ROrderSymbol, int RorderType,
+		double ROrderLots, double ROrderOpenPrice, __time64_t ROrderOpenTime,
+		double ROrderTakeProfit, double ROrderStopLoss, double  ROrderClosePrice, __time64_t  ROrderCloseTime,
+		__time64_t ROrderExpiration, double  ROrderProfit, double  ROrderCommission, double  ROrderSwap, wchar_t* ROrderComment) {
+
+		orders_old[ROrderTotal] = { ROrderTicket, Rmagic, L"default", RorderType, ROrderLots, ROrderOpenPrice, ROrderOpenTime, ROrderTakeProfit, ROrderStopLoss, ROrderClosePrice, ROrderCloseTime, ROrderExpiration, ROrderProfit, ROrderCommission, ROrderSwap, L"" };
+
+		wcscpy_s(orders_old[ROrderTotal].symbol, SYMBOL_LENGTH, ROrderSymbol);
+		wcscpy_s(orders_old[ROrderTotal].comment, COMMENT_LENGTH, ROrderComment);
+
+		std::wcout << "order #" << ROrderTotal << " " << ROrderTicket << " " << orders_old[ROrderTotal].symbol << "\r\n";
+		ordersRCount = ROrderTotal;
+		return orders_old[ROrderTotal].ticket;
+	}
+
+	void parseComment() {
+
+	}
+
+	int ffc_CompareOrders() {
+		int count = 0;
+		// сравнение (orders_old, orders) и создание структуры с действиями
+		for (int i = 0; i < ordersTotal; i++) {
+			parseComment();
+			if (orders[i].ticket == orders_old[i].ticket) { // тикет найден, сравниваем остальные данные
+				if (orders[i].tpprice != orders_old[i].tpprice || orders[i].slprice != orders_old[i].slprice) { // тикет изменен
+					count++;
+					std::wcout << "ticket find and is changed - " << orders_old[i].ticket << "\r\n";
+				}
+				else {
+					std::wcout << "ticket is original - " << orders_old[i].ticket << "\r\n";
+				}
+			} else if (orders[i].ticket < orders_old[i].ticket) { // тикет закрыть
+				count++;
+				std::wcout << "ticket is not find (close ticket) - " << orders_old[i].ticket << "\r\n";
+				actionOrder[count].action = 1;
+			} else { // тикет открыт вручную
+				std::wcout << "ticket is not find (ticket open) - " << orders_old[i].ticket << "\r\n";
+				actionOrder[count].action = 1;
+			}
+		}
+		return count;
+	}
+
+
 }
